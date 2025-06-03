20a6146876557effb900ddd1488528f0722e591d
Valdeci-cpd <valdeci@sergranbel.com.br>
2025-06-03 14:34:43 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+        return render_template('confirmacao.html', nome=nome, email=email, senha=senha)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
